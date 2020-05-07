0c07111ae4c733890229fcc7710e7ebc021e6d3f
Javier Oramas <javiale2000@gmail.com>
2020-05-07 13:39:50 -0400

    case sensitive bug fixed

--- script.py
+++ script.py
@@ -3,19 +3,28 @@ import shutil
 import os
 from os import path
 
+ext = ['.mpeg','.mp4','.avi','.mpg','.3gp','.webm', '.mov', '.mkv','.wmv', '.vob', '.ogg', '.ogv']
+
+def is_video(f):
+    for i in ext:
+        if(f.lower().endswith(i)):
+            return True
+    return False
 def extract_videos(source:str):
     
     os.makedirs('../Music_Videos',exist_ok=True)
     
+    
     for cp,dir,files in os.walk(source):
         for f in files:
             # print(f)
-            if f.endswith('.mpeg') or f.endswith('.mp4') or f.endswith('.avi') or f.endswith('.mpg'or f.endswith('.3gp'or f.endswith('.MOV')or f.endswith('.webm')or f.endswith('.MOV')or f.endswith('.mkv'))):
+            if is_video(f):
                 os.makedirs(path.join('../Music_Videos',cp[2:]),exist_ok=True)
                 # print(path.join('../Videos',cp[2:]))
                 shutil.move(path.join(cp,f),path.join('../Music_Videos',cp[2:]))
 
         # for cp,dir,files in os.walk(destination):
         #     for f in files:
+
 
 extract_videos('.')                
