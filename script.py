de8f0b8cba5e6920ad0ec553bd1f5477eb4f0140
Javier Oramas <javiale2000@gmail.com>
2020-05-08 01:24:05 -0400

    Custom Locations

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import zipfile
 import shutil
 import os
+import typer
 from os import path
+
+app = typer.Typer()
 
 ext = ['.mpeg','.mp4','.avi','.mpg','.3gp','.webm', '.mov', '.mkv','.wmv', '.vob', '.ogg', '.ogv']
 
@@ -10,21 +13,18 @@ def is_video(f):
         if(f.lower().endswith(i)):
             return True
     return False
-def extract_videos(source:str):
+
+@app.command(name='extract',help='extracts all videos in [Source Folder] to [Destination Folder]')
+def extract_videos(source:str, dest_folder:str):
     
-    os.makedirs('../Music_Videos',exist_ok=True)
-    
-    
+    os.makedirs('../'+dest_folder,exist_ok=True)
     for cp,dir,files in os.walk(source):
         for f in files:
-            # print(f)
             if is_video(f):
-                os.makedirs(path.join('../Music_Videos',cp[2:]),exist_ok=True)
-                # print(path.join('../Videos',cp[2:]))
-                shutil.move(path.join(cp,f),path.join('../Music_Videos',cp[2:]))
+                os.makedirs(path.join('../'+dest_folder,cp[2:]),exist_ok=True)
+                shutil.move(path.join(cp,f),path.join('../'+dest_folder,cp[2:]))
 
-        # for cp,dir,files in os.walk(destination):
-        #     for f in files:
+# extract_videos(source_folder,dest_folder)                
 
-
-extract_videos('.')                
+if __name__ == "__main__":
+    app()
