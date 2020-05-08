70106cab74e60adfc562328ec46f3ff10f66c1f5
Javier Oramas <javiale2000@gmail.com>
2020-05-08 02:02:41 -0400

    added other file formats to extract

--- script.py
+++ script.py
@@ -6,21 +6,24 @@ from os import path
 
 app = typer.Typer()
 
-ext = ['.mpeg','.mp4','.avi','.mpg','.3gp','.webm', '.mov', '.mkv','.wmv', '.vob', '.ogg', '.ogv']
+ext = {}
+ext['Video'] = ['.mpeg','.mp4','.avi','.mpg','.3gp','.webm', '.mov', '.mkv','.wmv', '.vob', '.ogg', '.ogv']
+ext['Audio'] = [ '.mp3', '.wav', '.aac', '.wma', '.dss', '.dvf', '.m4p', '.midi']
+ext['Pictures'] = ['.jpg', '.jpeg', '.png', '.jfif', '.exif', '.tiff', '.gif', '.gif', '.bmp', '.ppm', '.pgm', '.pbm', '.pnm', '.webp', '.heif', '.img', '.ico']
 
-def is_video(f):
-    for i in ext:
+def is_video(f, types):
+    for i in ext[types]:
         if(f.lower().endswith(i)):
             return True
     return False
 
-@app.command(name='extract',help='extracts all videos in [Source Folder] to [Destination Folder]')
-def extract_videos(source:str, dest_folder:str):
+@app.command(name='extract',help='extracts all [file_types] (Videos,Audio,Pictures) in [Source Folder] to [Destination Folder]')
+def extract_videos(types:str, source:str, dest_folder:str):
     
     os.makedirs('../'+dest_folder,exist_ok=True)
     for cp,dir,files in os.walk(source):
         for f in files:
-            if is_video(f):
+            if is_video(f,types):
                 os.makedirs(path.join('../'+dest_folder,cp[2:]),exist_ok=True)
                 shutil.move(path.join(cp,f),path.join('../'+dest_folder,cp[2:]))
         if len(os.listdir(cp) ) == 0:
